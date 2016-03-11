fbe1f83cb113871d284a05d38dc0be1744223d38
h4w5 <pj@ga.co>
2016-03-11 10:11:52 -0800

    rendering blabs

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,45 @@ function loadBlabs() {
 
   // 3. (6.) Finally, render/append the items.
   .then(
-    function(blabs) { console.log("Rendering: ", blabs); }
+    function(blabs) {
+      console.log("Rendering: ", blabs);
+
+      // A. Grab insertion point.
+      var $blabsList = $("#blabs-list");
+
+      // B. Store our template.
+      //   - 1. write the HTML outline.
+      //   - 2. write in the interpolated data.
+      var blabTemplate = `
+        <article class="blab blab-container">
+          <a href="/blabs/<%= _id %>"><h3><%= name %></h3></a>
+          <span>
+            Created at <%= createdAt %>, by
+            <a href="/users/<%= creator %>"><%= creatorHandle %></a>
+          </span>
+          <!-- <br> -->
+          <!-- <button>upvote</button> VoteCount <button>downvote</button> -->
+        </article>
+      `;
+
+      // C. Generate HTML!
+      //   - 1. compile the template into a render function.
+      //   - 2. run the object(s) through the render function.
+      var renderBlab = _.template(blabTemplate);
+
+      blabs.forEach(function(blab) {
+        var blabHtml = renderBlab(blab);
+
+        // D. ...
+
+        // E. Append the HTML to the DOM (at the insertion point)
+        $blabsList.append(blabHtml);
+      });
+
+
+
+
+    }
   )
 }
 
